135aec90890dc262265ef6f7bfaba31d290b3adc
HARSH CHAURASIYA <149835540+harshmnnit27@users.noreply.github.com>
2024-10-21 05:02:05 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 764694
+let accountEmail = "harsh@gmail,.com"
+var accountPassword ="12345"
+accountCity = "Sultanpur"
+ accountSt="up";
+
+// accountId =2
+
+accountEmail = "ayush@gmial.com"
+accountPassword ="232323"
+acccountCity = "allahabad"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,acccountCity ,accountSt]);
+
+
