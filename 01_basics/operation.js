6b24dddda047ec5bd53fc717b35cca656170d675
HARSH CHAURASIYA <149835540+harshmnnit27@users.noreply.github.com>
2024-10-21 05:30:39 +0000

    operationsbasics

---
+++ 01_basics/operation.js
@@ -0,0 +1,30 @@
+// // let value = 5
+// // let negValue = -value
+// // console.log(negValue)
+
+// // console.log(4+4)
+// // console.log(2**4)
+// // console.log(2%3)
+
+// let str1="hello"
+// let str2="harsh"
+// let str3= str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 4 + 6)
+// console.log(5 + 3 + "4")
+
+// console.log(+true)
+
+// Comparison 
+
+console.log(2>1)
+console.log("3">1)
+
+console.log(null>0)
+console.log(null>=0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined <0)
