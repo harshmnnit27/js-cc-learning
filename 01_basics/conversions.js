96731cf0c76b86ec1c24e528b364eecdd7fd69ca
HARSH CHAURASIYA <149835540+harshmnnit27@users.noreply.github.com>
2024-10-21 05:01:30 +0000

    conversionofdata type

---
+++ 01_basics/conversions.js
@@ -0,0 +1,12 @@
+let score =33
+
+console.log(typeof score);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true ; 0=> false
+// ""=> false
+// "hitesh"=> true
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,5 @@
+let name = "amrit"
+let age =18
+let isLoggesIn = false
+
+console.table([name,age,isLoggesIn]);
